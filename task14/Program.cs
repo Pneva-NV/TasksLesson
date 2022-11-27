0594193aff332ecd25df046ec3c5a0c2da8a7149
Natalia-Pneva <pneva_nv@mail.ru>
2022-11-27 15:12:27 +0300

    Кратно ли число 7 и 23

---
+++ task14/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int S = N % 7;
+int T = N % 23;
+int i = 0;
+if (i < S | i < T)
+{
+    Console.WriteLine($"не кратно");
+}
+else
+{
+     Console.WriteLine($"кратно");
+}
