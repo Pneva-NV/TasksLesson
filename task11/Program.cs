56f7e9d58551e28831197b1030d97bf691fbfef3
Natalia-Pneva <pneva_nv@mail.ru>
2022-11-27 13:51:36 +0300

    Удаляет вторую цифру из трехзначного числа

--- task11/Program.cs
+++ task11/Program.cs
@@ -3,3 +3,13 @@
 // 782 -> 72
 // 918 -> 98
 
+int N = new Random().Next(100, 1000);
+Console.WriteLine($"{N}");
+int C = N / 100;
+int C1 = C * 100;
+int D = N - C1;
+int D1 = D / 10;
+int D2 = D1 * 10;
+int E = D - D2;
+
+Console.WriteLine($"{C}{E}");
