4fb67f4c412797ca878c3b2172282c8136232edc
Natalia-Pneva <pneva_nv@mail.ru>
2022-11-27 12:56:47 +0300

    Выведение наибольшей цифры числа

---
+++ task9/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// Например:
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int N = new Random().Next(10, 99);
+Console.WriteLine($"{N}");
+int C = N / 10;
+int D = C * 10;
+int E = N - D;
+int max = C;
+
+int resolt = Math.Max(C, E);
+Console.WriteLine(resolt);
