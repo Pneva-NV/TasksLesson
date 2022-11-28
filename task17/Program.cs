9efadfc169218a420201a84883ce0a1516e4903c
Natalia-Pneva <pneva_nv@mail.ru>
2022-11-28 10:25:52 +0300

    Определение номера плоскости

---
+++ task17/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.Write("Введите значение x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int P1 = 1;
+if (x > 0 && y > 0)
+{
+    Console.WriteLine($"плоскость - {P1}");
+}
+int P2 = 2;
+if (x < 0 && y > 0)
+{
+    Console.WriteLine($"плоскость - {P2}");
+}
+int P3 = 3;
+if (x < 0 && y < 0)
+{
+    Console.WriteLine($"плоскость - {P3}");
+}
+int P4 = 4;
+if (x > 0 && y < 0)
+{
+    Console.WriteLine($"плоскость - {P4}");
+}
