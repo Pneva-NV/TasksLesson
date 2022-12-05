21f55b878dbe30ad2e0ed5b9a002cea73213a579
Natalia-Pneva <pneva_nv@mail.ru>
2022-12-05 15:04:09 +0300

    Вычисление суммы положительных/отрицательных чисел

--- task31/Program.cs
+++ task31/Program.cs
@@ -2,22 +2,22 @@
 // Найдите сумму отрицательных и положительных элементов массива.
 // Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
 
+int[] N = new int[12];
 int Sum1 = 0;
 int Sum2 = 0;
-for (int i = 1; i <= 12; i++)
+for (int i = 0; i < 12; i++)
 {
-    int N = new Random().Next(-9, 10);
-    Console.Write($"{N} ");
-    if (N >= 0)
+    N[i] = new Random().Next(-9, 10);
+    Console.Write($"{N[i]} ");
+    if (N[i] >= 0)
     {
-        Sum1 = Sum1 + N;
+        Sum1 = Sum1 + N[i];
     }
     else
     {
-        Sum2 = Sum2 + N;
+        Sum2 = Sum2 + N[i];
     }
 }
 Console.WriteLine();
 Console.WriteLine($"Сумма положительных чисел = {Sum1}");
 Console.WriteLine($"Сумма отрицательных чисел = {Sum2} ");
-
