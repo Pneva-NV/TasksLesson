517f27618bb8f7444c8570b0af53b3b61ecb3463
Natalia-Pneva <pneva_nv@mail.ru>
2022-12-15 09:54:51 +0300

    Поэлементное копирование массива

---
+++ task45/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] N = new int[6];
+int[] copy = new int[6];
+for (int i = 0; i < N.Length; i++)
+{
+    N[i] = new Random().Next(0, 10);
+    N = copy;
+    Console.Write($"{N[i]}-");
+    
+    Console.Write($"{copy[i]}  ");
+}
+
+
+
