d233c05a7fa21f769b8d562cd0105ff3b299825e
Natalia-Pneva <pneva_nv@mail.ru>
2022-12-30 10:49:37 +0300

    Вывод натуральных чисел в промежутке от N до 1

---
+++ task64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите любое положительное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int A = 1;
+string NatNum(int A, int N)
+{
+    if (A <= N) return NatNum(A + 1, N) + $"{A} ";
+    else return String.Empty;
+}
+string U=NatNum(A, N);
+U = U.Replace(" ", ", ");
+U = U.Replace($"{A},", $"{A}");
+Console.Write($"N = {N} -> \" ");
+Console.Write(U);
+Console.Write("\"");
